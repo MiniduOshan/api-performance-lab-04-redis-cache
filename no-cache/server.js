e7fba31e39ccfacb5daccbf510a32e54d6e9a764
Minidu Oshan <126508461+MiniduOshan@users.noreply.github.com>
2026-05-07 11:20:53 +0530

    update result file

--- no-cache/server.js
+++ no-cache/server.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "",
-  database: "performance_lab_03",
+  database: "perfo_lab_03",
 });
 
 app.get("/products", async (req, res) => {
